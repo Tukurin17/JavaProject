39c9f77e06ba316917a722630fbf856f2f2229bb
Ren0X <alexyanamusicpro@gmail.com>
2020-10-22 10:02:36 +0200

    Añadido metodo recursivo al factorial para comprobar la eficiencia del programa

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,10 +1,26 @@
 public class Factorial {
     public static void main (String[] args) {
-        int n, f=1;
+        double n;
         n=ES.leeN("Introduce el numero del que quieres calcular el Factorial: ");
+        double factorial=iterativo(n);
+        double factorialr=recursivo(n);
+        System.out.println("El factorial iterativo de "+Math.round(n)+" es "+Math.round(factorial));
+        System.out.println("El factorial recursivo de "+Math.round(n)+" es "+Math.round(factorialr));
+    }
+    static double iterativo(double n) {
+        double f=1;
         for (int i=1;i!=n+1;i++) {
             f=f*i;
         }
-        System.out.println("El factorial de "+n+" es "+f);
+        return f;
+    }
+
+    static double recursivo(double n) {
+        if (n==0) {
+            return 1;
+        }
+        else {
+            return n*(recursivo(n-1));
+        }
     }
 }
