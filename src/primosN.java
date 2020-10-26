26348afdf32f8de85909997dcde10c86f1fb00a5
Ren0X <alexyanamusicpro@gmail.com>
2020-10-26 11:05:35 +0100

    Arreglado primos hasta N y añadido N primos

--- src/primosN.java
+++ src/primosN.java
@@ -1,16 +1,9 @@
-import java.util.*;
 public class primosN {
     public static void main(String[] args) {
-        Scanner teclado= new Scanner (System.in);
-        int d=2;
-        int n=2;
-        System.out.println("Introduzca un número: ");
-        int numero= teclado.nextInt();
-        while (n!=numero) {
-            if ((n%d)!=0) {
-                System.out.println(n);
-            }
-            n=n+1;
+        int d, i;
+        ES.leeN("Introduce un numero hasta donde llegar: ");
+        for (int n=2;n!=i;n++) {
+            
         }
     }
 }
