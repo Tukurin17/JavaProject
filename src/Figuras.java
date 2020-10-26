229567dc4b8b4e16233ccd46c88fc49759a674b6
Ren0X <alexyanamusicpro@gmail.com>
2020-10-26 14:40:43 +0100

    Ciclos para pintar figuras todas introducidas en un menu y con metodos separados

---
+++ src/Figuras.java
@@ -0,0 +1,40 @@
+public class Figuras {
+    public static void main(String[] args) {
+        int opcionElegida = pintaMenu("Figuras", "Cuadrado", "Rectangulo","Triangulo", "Rombo");
+        switch(opcionElegida) {
+            case 1:
+                int n;
+                n=ES.leeN("Introduce las filas del cuadrado: ");
+                char c=ES.leecaracter("Introduce el caracter: ");
+                cuadrado(n,c);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                System.err.println("La intruccion elegida no existe en el programa!");
+                ES.debug(4);
+        }
+    }
+    static int pintaMenu(String titulo, String op1, String op2, String op3, String op4) {
+        System.err.println("--" + titulo + "--");
+        System.out.println("1.-" + op1);
+        System.out.println("2.-" + op2);
+        System.out.println("3.-" + op3);
+        System.out.println("4.-" + op4);
+        System.out.println("---------------");
+        int opcion = ES.leeN("Introduce la opcion: ");
+        return opcion;
+    }
+    static void cuadrado(int n, char c) {
+        for (int i=0;i!=n;i++) {
+            System.out.println();
+            for (int j=0;j!=n;j++) {
+                System.out.print(c);
+            }
+        }
+    }
+}
