c224541db481954de1d1d7d25a7e32aec045c270
Ren0X <alexyanamusicpro@gmail.com>
2020-10-30 10:53:46 +0100

    Optimizado NotasAlumnos

--- src/NotasAlumnos.java
+++ src/NotasAlumnos.java
@@ -6,35 +6,25 @@ public class NotasAlumnos {
         String[] nombre=new String[i];
         int[] nota=new int[i];
         int k=i;
-        System.out.println("------------------------------------");
         System.out.println("Puedes terminar antes el programa si en el nombre del alumno escribes salir");
-        System.out.println("------------------------------------");
         ES.leeDeTeclado("Pulsa \"ENTER\" para continuar...");
         do {
             do {
-                System.out.println("------------------------------------");
                 System.out.println("El nombre del alumno no puede estar vacio.");
-                System.out.println("------------------------------------");
                 nombre[x]=ES.leeDeTeclado("Introduce el nombre del alumno "+(x+1)+": ");
-                System.out.println("------------------------------------");
             } while (nombre[x].equals(""));
             if (!nombre[x].equals("salir")) {
                 do {
                     System.out.println("La nota maxima solo puede ser de "+n);
-                    System.out.println("------------------------------------");
                     if (!nombre[x].equals("")){nota[x]=ES.leeN("Introduce la nota del alumno "+(x+1)+": ");}
-                    System.out.println("------------------------------------");
                 } while (nota[x]>n);
             }
             else {
                 k=x;
-                x=nombre.length-1;
+                x=i-1;
             }
             x++;
-        } while (x!=nombre.length);
-        System.out.println("------------------------------------");
-        System.out.println("Has registrado el numero maximos de alumnos o has decidido salir antes, este es le resultado:");
-        System.out.println("------------------------------------");
+        } while (x!=i);
         System.out.println("NOMBRE -- NOTA");
         System.out.println("--------------");
         for (int j=0;j!=k;j++) {
